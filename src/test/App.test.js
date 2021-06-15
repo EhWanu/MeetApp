3c5fe09ed9ca31331b845a14cba90fe329a4686e
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-06-15 14:37:47 -0400

    4.4 task push

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberofEvents from '../NumberofEvents';
 
 describe('<App /> component', () => {
 
@@ -21,7 +22,18 @@ describe('<App /> component', () => {
     test('render CitySearch', () => {
       expect(AppWrapper.find(CitySearch)).toHaveLength(1);
     });
+
+
+    test('renders the NumberofEvents Bar', () => {
+      expect(AppWrapper.find(NumberofEvents)).toHaveLength(1);
+    });
+
+
+
   });
+
+
+  
 
 
 });
