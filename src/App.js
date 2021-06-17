9d5676012917d2096fd014fe150cb1c88bf83fd6
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-06-17 12:32:39 -0400

    updateEvents is not defined?

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { getEvents } from './api';
 
 class App extends Component {
   state = {
@@ -12,12 +13,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents />
         <EventList events={this.state.events} />
       </div>
     );
   }
 }
+
+
+
+updateEvents = (location) => {
+  getEvents().then((events) => {
+    const locationEvents = events.filter((event) => event.location === location);
+    this.setState({
+      events: locationEvents
+    });
+  });
+}
+
+
 
 export default App;
