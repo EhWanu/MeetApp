d087331afc6ca8d7744e15e5b806639d58350209
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-06-18 15:28:18 -0400

    final tests and stylings

--- src/test/CitySearch.test.js
+++ src/test/CitySearch.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
-import { mockData } from "../mock-data";
-import { extractLocations } from "../api";
+import {  mockData } from '../mock-data';
+import { extractLocations } from '../api';
 
 describe('<CitySearch /> component', () => {
   let locations, CitySearchWrapper;
   beforeAll(() => {
     locations = extractLocations(mockData);
+    CitySearchWrapper = shallow(<CitySearch />);
     CitySearchWrapper = shallow(<CitySearch locations={locations} updateEvents={() => {}} />);
   });
 
@@ -15,7 +16,7 @@ describe('<CitySearch /> component', () => {
     expect(CitySearchWrapper.find('.city')).toHaveLength(1);
   });
 
-  test('render a list of suggestions', () => {
+  test('renders a list of suggestions', () => {
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
   });
 
@@ -28,15 +29,14 @@ describe('<CitySearch /> component', () => {
     CitySearchWrapper.setState({
       query: 'Munich'
     });
-    const eventObject = { target: { value: 'Berlin' } };
+    const eventObject = { target: { value: 'Berlin' }};
     CitySearchWrapper.find('.city').simulate('change', eventObject);
     expect(CitySearchWrapper.state('query')).toBe('Berlin');
   });
 
   test('render list of suggestions correctly', () => {
-    const locations = extractLocations(mockData);
     CitySearchWrapper.setState({ suggestions: locations });
-    const suggestions = CitySearchWrapper.state("suggestions");
+    const suggestions = CitySearchWrapper.state('suggestions');
     expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
     for (let i = 0; i < suggestions.length; i += 1) {
       expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
@@ -45,23 +45,23 @@ describe('<CitySearch /> component', () => {
 
   test('suggestion list match the query when changed', () => {
     CitySearchWrapper.setState({ query: '', suggestions: [] });
-    CitySearchWrapper.find('.city').simulate('change', { target: { value: "Berlin" }, });
-    const query = CitySearchWrapper.state("query");
+    CitySearchWrapper.find('.city').simulate('change', {
+      target: { value: 'Berlin' },
+    });
+    const query = CitySearchWrapper.state('query');
     const filteredLocations = locations.filter((location) => {
       return location.toUpperCase().indexOf(query.toUpperCase()) > -1;
     });
-    expect(CitySearchWrapper.state("suggestions")).toEqual(filteredLocations)
+    expect(CitySearchWrapper.state('suggestions')).toEqual(filteredLocations);
   });
 
   test('selecting a suggestion should change query state', () => {
     CitySearchWrapper.setState({
-      query: 'Berlin'
-    });
+      query: 'Berlin'  });
     const suggestions = CitySearchWrapper.state('suggestions');
     CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
-    expect(CitySearchWrapper.state('query')).toBe(suggestions[0]);
+    expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
   });
-
 
   test("selecting CitySearch input reveals the suggestions list", () => {
     CitySearchWrapper.find('.city').simulate('focus');
@@ -69,7 +69,6 @@ describe('<CitySearch /> component', () => {
     expect(CitySearchWrapper.find('.suggestions').prop('style')).not.toEqual({ display: 'none' });
   });
 
-  
   test("selecting a suggestion should hide the suggestions list", () => {
     CitySearchWrapper.setState({
       query: 'Berlin',
@@ -80,5 +79,12 @@ describe('<CitySearch /> component', () => {
     expect(CitySearchWrapper.find('.suggestions').prop('style')).toEqual({ display: 'none' });
   });
 
-
-})
+  test("suggestions list will appear upon having a focus on city input field", () => {
+    CitySearchWrapper.setState({
+      query: '',
+      suggestions: locations,
+    });
+    CitySearchWrapper.find('.city').simulate('focus');
+    expect(CitySearchWrapper.find('.suggestions').prop('style')).toEqual({});
+  });
+});
