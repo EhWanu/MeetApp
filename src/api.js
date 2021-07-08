90a1c455ad6e59d500de1b1ea09a4982997b7984
EhWanu <77857074+EhWanu@users.noreply.github.com>
2021-07-08 13:22:12 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -100,7 +100,7 @@
  const getToken = async (code) => {
    const encodeCode = encodeURIComponent(code);
    const { access_token } = await fetch(
-     `https://gtujk48fb7.execute-api.us-east-2.amazonaws.com/dev/api/token${encodeCode}`
+     `https://gtujk48fb7.execute-api.us-east-2.amazonaws.com/dev/api/token/${encodeCode}`
    )
      .then((res) => {
        return res.json();
