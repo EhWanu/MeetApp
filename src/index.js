a2028cd315ad5107018c9ca0128f7b254a768bc3
EhWanu <cameronjshadgett@gmail.com>
2021-07-13 15:19:02 -0400

    offline capability

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
 
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -18,11 +16,13 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.unregister();
 serviceWorkerRegistration.register();
 
-
+// config atatus
 atatus.config('06fa315d0e5b49e78979fafe64fc3703').install();
 
+// atatus.notify(new Error('Test Atatus Setup'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
